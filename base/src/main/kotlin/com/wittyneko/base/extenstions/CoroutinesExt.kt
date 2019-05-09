e48bedfb91a2de37109973ef20ba208521ca5cac
wittyneko <wittytutu@gmail.com>
2019-05-09 16:06:52 +0800

    兼容2.x协程

--- base/src/main/kotlin/com/wittyneko/base/extenstions/CoroutinesExt.kt
+++ base/src/main/kotlin/com/wittyneko/base/extenstions/CoroutinesExt.kt
@@ -1,9 +1,13 @@
-package com.wittyneko.base.extenstions
+@file:Suppress("NOTHING_TO_INLINE")
 
-import kotlinx.coroutines.experimental.*
-import kotlinx.coroutines.experimental.android.UI
-import kotlin.coroutines.experimental.CoroutineContext
-import kotlin.coroutines.experimental.EmptyCoroutineContext
+package com.wittyneko.myapplication
+
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.*
+import kotlinx.coroutines.android.asCoroutineDispatcher
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 
 /**
@@ -14,34 +18,38 @@ import kotlin.coroutines.experimental.EmptyCoroutineContext
 
 @PublishedApi
 internal val POOL = newFixedThreadPoolContext(2 * Runtime.getRuntime().availableProcessors(), "bg")
+val UI = Handler(Looper.getMainLooper()).asCoroutineDispatcher("UI")
 
-inline suspend fun <T> run(context: CoroutineContext
-                           , noinline block: suspend () -> T
-) = kotlinx.coroutines.experimental.run(context, block)
 
-inline fun <T> runBlocking(context: CoroutineContext = EmptyCoroutineContext
-                           , noinline block: suspend CoroutineScope.() -> T
-) = kotlinx.coroutines.experimental.runBlocking(context, block)
+inline suspend fun <T> run(
+        context: CoroutineContext
+        , noinline block: suspend () -> T
+) = withContext(context) { block() }
+
+inline fun <T> bg(crossinline block: () -> T): Deferred<T> = bgAsync { block() }
+
+inline fun <T> runBlocking(
+        context: CoroutineContext = EmptyCoroutineContext
+        , noinline block: suspend CoroutineScope.() -> T
+) = kotlinx.coroutines.runBlocking(context, block)
 
 inline fun launch(
         context: CoroutineContext,
         start: CoroutineStart = CoroutineStart.DEFAULT,
         noinline block: suspend CoroutineScope.() -> Unit
-) = kotlinx.coroutines.experimental.launch(context, start, block)
+) = GlobalScope.launch(context, start, block)
 
 inline fun <T> async(
         context: CoroutineContext,
         start: CoroutineStart = CoroutineStart.DEFAULT,
         noinline block: suspend CoroutineScope.() -> T
-) = kotlinx.coroutines.experimental.async(context, start, block)
-
-inline fun <T> bg(crossinline block: () -> T): Deferred<T> = org.jetbrains.anko.coroutines.experimental.bg(block)
+) = GlobalScope.async(context, start, block)
 
 inline fun uiLaunch(
         start: CoroutineStart = CoroutineStart.DEFAULT,
         noinline block: suspend CoroutineScope.() -> Unit
 ) = launch(UI, start, block)
 
-inline fun <T> uiAsync(noinline block: suspend CoroutineScope.() -> T) = async(UI, block = block)
+inline fun <T> uiAsync(noinline block: suspend CoroutineScope.() -> T) = async(Dispatchers.Main, block = block)
 
 inline fun <T> bgAsync(noinline block: suspend CoroutineScope.() -> T) = async(POOL, block = block)
